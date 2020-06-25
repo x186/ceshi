ecef471aeaa6962b8e212b6908316d38c2fbc6cf
x186 <3302293098@qq.com>
2020-06-25 14:34:06 +0800

    ceshi

---
+++ hh.py
@@ -0,0 +1,16 @@
+msg = "vfvdfv"
+print(msg)
+print(msg.title())
+print(" vdfv ")
+name = "add love"
+print(name.lstrip())
+"""kgoergop"""
+age = 18
+weight = 75.23
+str_name = "xiaoming       "
+print("%s的年龄是%03d岁，体重是%.2fkg" % (str_name.rstrip().title(), age, weight))
+bicycles = ['trek', '18', 'redline', 'specialized']
+print(bicycles[0]+ bicycles[1])
+bicycles.insert(0, "app")
+print(f"列表里有{bicycles}",end="\ngg")
+print(type(bicycles[1]))
